f779c357d5cf092ae24b8e5553794b79fec1858e
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-06 16:22:44 +0100

    Install workflows for GitHub

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: ["xo", "prettier"],
   overrides: [
     {
-      extends: [["xo-typescript", "prettier"]],
+      extends: ["xo-typescript", "prettier"],
       files: ["*.ts", "*.tsx"],
     },
   ],
