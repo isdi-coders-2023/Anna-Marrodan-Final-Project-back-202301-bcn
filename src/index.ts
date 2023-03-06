f2da1eab132304551ee547bc1d050d41cb0bf994
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-06 21:24:15 +0100

    Create startServer function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import "./loadEnvironment.js";
 import { connectDatabase } from "./database/connectDatabase.js";
 import mongoose from "mongoose";
+import { startServer } from "./server/startServer.js";
 
+const port = process.env.PORT ?? 4001;
 const mongooseUrl = process.env.MONGOOSE_URL!;
 
 mongoose.set("toJSON", {
@@ -13,3 +15,4 @@ mongoose.set("toJSON", {
 });
 
 await connectDatabase(mongooseUrl);
+await startServer(+port);
--- src/server/startServer.ts
+++ src/server/startServer.ts
@@ -2,3 +2,12 @@ import createDebug from "debug";
 import { app } from "./app.js";
 
 const debug = createDebug("users:startServer");
+
+export const startServer = async (port: number) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(`Server listening on  http://localhost:${port} `);
+
+      resolve(server);
+    });
+  });
