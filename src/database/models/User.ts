d084c080147f5263a2922c327efdfa954662a29c
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-07 13:41:38 +0100

    Create Schema and model for user

---
+++ src/database/models/User.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+export const loginUserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+});
+
+export const User = model("User", loginUserSchema, "users");
