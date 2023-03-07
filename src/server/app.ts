7ad5a45ff6083d74bb74b8dd1828e0676443f695
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-07 22:33:11 +0100

    Add notFoundError and generalError routes

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,6 +3,10 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { usersRouter } from "../router/usersRouter.js";
+import {
+  generalError,
+  notFoundError,
+} from "./middlewares/errorMiddlewares/errorMiddlewares.js";
 
 export const app = express();
 
@@ -24,3 +28,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/users", usersRouter);
+
+app.use(notFoundError);
+app.use(generalError);
