d0c1e9e173159ff218cccf17bd743c025537ba39
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-09 10:54:02 +0100

    Handle errors when connecting to data base

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import "./loadEnvironment.js";
 import { connectDatabase } from "./database/connectDatabase.js";
 import { startServer } from "./server/startServer.js";
+import createDebug from "debug";
+
+const debug = createDebug("plantips");
 
 const port = process.env.PORT ?? 4001;
 const mongooseUrl = process.env.MONGOOSE_URL!;
 
-await connectDatabase(mongooseUrl);
-await startServer(+port);
+try {
+  await connectDatabase(mongooseUrl);
+  await startServer(+port);
+} catch (error) {
+  debug(error);
+}
