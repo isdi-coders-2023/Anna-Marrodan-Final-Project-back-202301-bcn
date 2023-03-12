ac3aa20a40ada5dc4438f1e264d34ef5c2049b1f
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-12 16:05:06 +0100

    Create getTips controller

---
+++ src/server/controllers/tipControllerss/tipControllers.ts
@@ -0,0 +1,23 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { CustomError } from "../../../CustomError/CustomError";
+import { Tip } from "../../../database/models/Tip";
+
+export const getTips = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const tips = await Tip.find().exec();
+
+    res.status(200).json({ tips });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Couldn't retrieve tips"
+    );
+
+    next(customError);
+  }
+};
